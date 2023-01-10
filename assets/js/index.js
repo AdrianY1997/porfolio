c17fa55a2d6b6e42e895aef8bea05cbc93e4bd96
HappyFox.Devs <yasnoadriandatos@gmail.com>
2023-01-10 12:33:39 -0500

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
 const date = document.querySelector("#date")
 
 date.innerHTML = new Date().getFullYear();
+
+// Collapse 
+
+const collapseCollection = document.querySelectorAll("*[data-hf-function='collapse']")
+
+for (item of collapseCollection) {
+    item.addEventListener("click", (t) => {
+        for (child of item.children) {
+            if (child.classList.contains("collapse")) {
+                child.classList.toggle("hide")
+                child.classList.toggle("show")
+                child.style.height =
+                    child.classList.contains("hide") 
+                    ? "0" 
+                    : "auto"
+            }
+        }
+    })
+}
